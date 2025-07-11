47a73c45394c04b537b411ad5bf137f5c99e2b08
August <augustogatticorrea777@gmail.com>
2025-07-11 19:50:33 -0400

    Alteração da logica de spawn dos cactos

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,18 +1,19 @@
 import random
-
+import pygame
 from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 
 class Cactus(Obstacle):
-
-    CACTUS = [
-        (LARGE_CACTUS, 300),
-        (SMALL_CACTUS, 325),
-    ]
-
     def __init__(self):
-        image, cactus_pos = self.CACTUS[random.randint(0, 1)]
-        self.type = random.randint(0, 2)
-        super().__init__(image, self.type)
-        self.rect.y = cactus_pos
+        if random.randint(0, 1) == 0:
+            cactus_list = SMALL_CACTUS
+            y_pos = 365
+        else:
+            cactus_list = LARGE_CACTUS
+            y_pos = 340
+
+        image = random.choice(cactus_list)
+
+        super().__init__(image, 0)
+        self.rect.y = y_pos
